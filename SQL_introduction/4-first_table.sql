d1d76bf178abd75407ea9251fed11677a13f4a0d
Daezy-codergirl <122308807+Daezy-codergirl@users.noreply.github.com>
2023-08-17 14:11:49 +0100

    first table

---
+++ SQL_introduction/4-first_table.sql
@@ -0,0 +1,11 @@
+-- create a database
+
+CREATE SCHEMA IF NOT EXISTS hbtn_0c_0;
+
+-- use database name
+
+USE hbtn_0c_0;
+
+-- create a first table
+
+CREATE TABLE first_table (id INT, name VARCHAR(256));
