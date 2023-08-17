cb976c6f3ed1bd9e24ea3ffeb71501400851e965
Daezy-codergirl <122308807+Daezy-codergirl@users.noreply.github.com>
2023-08-17 12:01:10 +0100

    list tables

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -1,18 +1,7 @@
--- create database
-
-CREATE SCHEMA IF NOT EXISTS hbtn_test_db_0;
-
--- use database name
-
-USE hbtn_test_db_0;
-
--- create 3 tables and add atleast one column
-
-CREATE TABLE holbteron_0 (studentId int(10));
-CREATE TABLE holbteron_1 (studentName varchar(100));
-CREATE TABLE holbteron_2 (country varchar(50));
-
 -- show a list of available tables
 
-SHOW tables;
+SHOW tables in hbtn_0c_0;
+
+
+
 
